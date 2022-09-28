613cc4817488399d6e3b292df6baea3fa466aec2
Julian Cardone <juliancardone1@gmail.com>
2022-09-28 13:58:03 -0400

    working!

---
+++ recursion.rb
@@ -0,0 +1,27 @@
+def range(min, max)
+    new_arr = []
+    (min...max).each do |n|
+        new_arr << n 
+    end
+    new_arr
+end
+
+def range_r(min, max)
+    return [] if min >= max
+
+    [min] + range_r(min + 1, max)
+end
+
+# p range_r(1, 5)
+
+def expo1(b, n)
+    return 1 if n == 0
+    b * expo1(b, n-1)
+end
+
+def expo2(b, n)
+    return 1 if n == 0
+    
+end
+
+#p expo1(2, 2)
